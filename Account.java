09d4549544f19a3e4cecdef7d9de7ce30e021ba1
Amna Tiwana <amnatiwana2015@gmail.com>
2020-07-14 15:10:35 -0700

    I worked on the foundation for the Account class.

--- Account.java
+++ Account.java
@@ -1,8 +1,44 @@
-
 import.
-public class Account {
-
-	private String name;
+public class Account 
+{
+	//variables 
+	private String name, userName, password, email, securityAnswer; 
+	//private String id; 
 	
+
+	//methods
+	
+
+	public void setName(String n)
+	{
+		name = n; 
+	}
+	public String getName() {return name;}
+	
+
+	
+
+	public void setUserName(String un)
+	{
+		userName = un; 
+	}
+	public String getName() {return userName;}
+	
+
+	
+
+	public void setPassword(String p)
+	{
+		password = p; 
+	}
+	//public String getPassword() {return password;}
+
+	
+
+	public void setEmail(String e)
+	{
+		email = e; 
+	}
+	public String getEmail() {return email;}
 	
 }
