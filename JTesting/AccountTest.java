d64f52d7d8f20ffed813fabc3df118103737c993
Amna Tiwana <amnatiwana2015@gmail.com>
2020-07-15 15:45:59 -0700

    I am working on the tests for the Account class.

--- JTesting/AccountTest.java
+++ JTesting/AccountTest.java
@@ -8,5 +8,32 @@ class AccountTest {
 	void test() {
 		fail("Not yet implemented"); // TODO
 	}
+	
+	@Test
+	void testEmail()
+	{
+		//This should test if the email is an active email.
+	}
+	
+	@Test
+	void testPassword()
+	{
+		//The length of the password should be 8 characters with a number 
+		//and a special character.
+	}
+	
+	@Test 
+	void testUsername()
+	{
+		//This should check if the username already exists or not.
+	}
+	
+	@Test
+	void testResetPassword()
+	{
+		//The reset password function will send an email to the user if they need 
+		//to reset their passsword.
+		//This will test if 
+	}
 
 }
