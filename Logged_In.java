5771a2242cd7f96f7dc69751c8039bbdce420a49
maoui810 <60634367+maoui810@users.noreply.github.com>
2020-07-21 14:29:25 -0600

    Login gui work

--- Logged_In.java
+++ Logged_In.java
@@ -1,12 +1,94 @@
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
-public class Logged_In {
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.*;
+public class Logged_In extends JDialog {
+	
+
+	public JTextField usernameText;
+	private JPasswordField passText;
+	
+	private JLabel UsName;
+	private JLabel PsPass;
+	
+	private JButton Login;
+	private JButton SignUp;
 	
 	
-	public static void main(String[] args)
-	{
+	private boolean check;
+	
+	
+	
+	public Logged_In(Frame parent,String check) {
+		
+		if(check.equals("login")) {
+			 login(parent);
+		}
+		else
+		{
+			singup(parent);
+		}
+	
+		 
+		
+		
+		
 		
 		
 		
 	}
+
+
+
+	private void singup(Frame parent) {
+		// TODO Auto-generated method stub
+		
+		JPanel p = new JPanel(new GridBagLayout());
+		GridBagConstraints ccsss = new GridBagConstraints();
+		
+		ccsss.fill = GridBagConstraints.HORIZONTAL;
+		
+		UsName = new JLabel("Username: ");
+		ccsss.gridx=0;
+		ccsss.gridy=0;
+		ccsss.gridwidth=1;
+		
+		p.add(UsName, ccsss);
+		
+		usernameText= new JTextField(30);
+		ccsss.gridx=1;
+		ccsss.gridy=0;
+		ccsss.gridwidth=2;
+		p.add(usernameText,ccsss);
+		
+		
+		PsPass= new JLabel("Password: ");
+		ccsss.
+		
+	}
+
+
+
+	private void login(Frame parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
 
 }
