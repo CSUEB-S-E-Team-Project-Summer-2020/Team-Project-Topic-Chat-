18c70eefb555f4c44525f646e60e14b78df6ca82
Amna Tiwana <amnatiwana2015@gmail.com>
2020-07-25 14:33:48 -0700

    updated Account class

--- Account.java
+++ Account.java
@@ -1,72 +1,67 @@
 package ChatSysetm;
 
 
-public class Account 
+public class Account
 {
-	//variables 
-	private String userName, password, securityAnswer; 
-	private String status;
-	//private String id; 
-	
-	//methods
+//variables
+private String userName, password, securityAnswer;
+private String status;
+//private String id;
 
-	
-	
-	public void setUserName(String un)
-	{
-		this.userName = un; 
-	}
-	public String getUserName() 
-	{
-		return userName;
-	}
-	public void setPassword(String pw)
-	{
-		this.password = pw; 
-		
-	}
-	public String getPassword() {
-		
-		return password;
-	}
+//methods
 
-	
 
-	public boolean setSecurityAnswer(String answer)
-	{
-		this.securityAnswer=answer;
-		return true;
-		
-	}
-	public String getSecurityAnswer()
-	{
-		
-		return securityAnswer;
-	}
-	public void setStatusOnline() {
-		
-		this.status="online";
-	}
-	public void setStatusOffline() {
-		
-		this.status="offline";
-	}
-	public void setStatusBlocked() {
-		
-		this.status="blocked";
-	}
-	public String getStatus()
-	{
-	
-		return status;
-		
-	}
+
+public void setUserName(String un)
+{
+this.userName = un;
+}
+public String getUserName()
+{
+return userName;
+}
+public void setPassword(String pw)
+{
+this.password = pw;
+
+}
+public String getPassword() {
+
+return password;
+}
+
+
+
+public boolean setSecurityAnswer(String answer)
+{
+this.securityAnswer=answer;
+return true;
+
+}
+public String getSecurityAnswer()
+{
+
+return securityAnswer;
+}
+public void setStatusOnline() {
+
+this.status="online";
+}
+public void setStatusOffline() {
+
+this.status="offline";
+}
+public void setStatusBlocked() {
+
+this.status="blocked";
+}
+public String getStatus()
+{
+
+return status;
 
 }
 
-	
-	
-	
-	
+}
 
 
