3768f63e6e13c06c76e24932466044f40ea24d07
Amna Tiwana <amnatiwana2015@gmail.com>
2020-07-25 15:47:46 -0700

    Started the Delete class..

---
+++ Delete.java
@@ -0,0 +1,15 @@
+
+public class Delete extends ServerSide {
+	private String username;
+	private String password;
+	private String sAnswer;
+	public Account accountObj = new Account();
+	
+	private void deleteUsername(String username, String password, String sAnswer)
+	{
+		accountObj.setUserName();
+		accountObj.setPassword();
+		accountObj.setSAnswer();
+		
+	}
+}
